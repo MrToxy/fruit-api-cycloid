a63824cccc9f95e72ecc2d918839678fecc6e6e8
joão Petinga <joao.petinga@gmail.com>
2021-04-08 01:05:24 +0100

    add test coverage

--- src/utils/normalize.worker.js
+++ src/utils/normalize.worker.js
@@ -3,8 +3,6 @@ export default async (fruitsObject) => {
   return fruits;
 };
 
-// unfortunately web workers do not cope well with functions because they cant clone them properly
-// in this case DRY < performance since its a small piece of code to maintain
 function flatten(object, all = []) {
   if (typeof object === "object" && !Array.isArray(object)) {
     Object.values(object).forEach((value) => {
